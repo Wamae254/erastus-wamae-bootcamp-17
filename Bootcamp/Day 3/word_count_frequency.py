cdb6988739ffc4be8a0a6d77149b47f78f58ba82
Wamae254 <wamaekariuki4@gmail.com>
2017-04-05 19:05:02 +0300

    Delete word_count_frequency.py

--- Bootcamp/Day 3/word_count_frequency.py
+++
@@ -1,9 +0,0 @@
-wordstring = 'Run if you can run faster if they can not'
-
-wordlist = wordstring.split()
-
-wordfreq = []
-for w in wordlist:
-    wordfreq.append(wordlist.count(w))
-
-print("List\n" + str(wordlist) +  str(wordfreq) +"\n")
